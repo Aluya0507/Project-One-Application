20f9d942bd5d64823b8b472b317d1bd97d55042c
raceice19 <56613698+raceice19@users.noreply.github.com>
2020-09-29 15:09:17 -0500

    Create UserInterface.java

---
+++ UserInterface.java
@@ -0,0 +1,77 @@
+// --== CS400 File Header Information ==--
+// Name: Kairas Mistry
+// Email: kbmistry@wisc.edu
+// Team: EB
+// TA: Keren Chen
+// Lecturer: Gary Dahl
+// Notes to Grader: <optional extra notes>
+
+import java.util.Scanner;
+
+//This class is the front end portion of the project
+public class UserInterface {
+  
+  private static void options(){
+    System.out.println("Welcome to the Worldwide Directory.");
+    System.out.println("What would you like to do?");
+    System.out.println("(V)iew current directory");
+    System.out.println("(C)lear the current directory");
+    System.out.println("(E)nter a new country");
+    System.out.println("(A)dd info to an existing country");
+    System.out.println("(G)et info about a country");
+    System.out.println("(R)emove a country");
+    System.out.println("(L)oad default directory");
+    System.out.println("(Q)uit");
+  }
+  
+  public static void main(String[] args) {
+    StateTable map = new StateTable();
+    Scanner sc = new Scanner(System.in);
+    String input;
+    String info;
+    int index;
+    
+    do {
+      options();
+      input = sc.nextLine().substring(0, 1).toLowerCase();
+      switch(input) {
+        case "v": //TODO
+          System.out.println(map.toString());
+          break;
+        case "c":
+          map.clear();
+          break;
+        case "e":
+          System.out.println("Which country would you like add?");
+          input = sc.nextLine();
+          map.keyToIndex(input);
+          break;
+        case "a":
+          System.out.println("Which existing country would you like to add information to?");
+          input = sc.nextLine();
+          System.out.println("What information would you like to add?");
+          info = sc.nextLine();
+          map.insertInfo(input, info);
+          break;
+        case "g": //TODO
+          System.out.println("Which existing country would you like to know about?");
+          input = sc.nextLine();
+          index = map.keyToIndex(input);
+          
+          break;
+          
+        case "r":
+          System.out.println("Which existing country would you like to remove?");
+          input = sc.nextLine();
+          map.remove(input);
+          break;
+        case "l":
+          break;
+        
+      }
+    }while(!input.equals("q"));
+    
+    
+    
+  }
+}
